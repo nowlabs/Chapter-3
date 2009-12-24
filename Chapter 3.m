d4370f22f898156ebf615285400a816202ecfb4f
Thomas Eapen <thomas@nowlabs.net>
2009-12-24 00:55:29 -0500

    Reading the words from a file now

--- Chapter 3.m
+++ Chapter 3.m
@@ -1,11 +1,12 @@
 #import <Foundation/Foundation.h>
 
 int main (int argc, const char * argv[]) {
-    const char *words[] = {"aadvark", "abacus", "allude", "zygote"};
-	int wordCount = 4;
-	int i;
-	for	(i = 0; i < wordCount; i++) {
-		NSLog(@"%s is %d character(s) long, the first character is %c", *(words + i), strlen(words[i]), **(words + i));
+	FILE *wordFile = fopen("/tmp/words.txt", "r");
+	char word[100];
+	while (fgets(word, 100, wordFile)) {
+		word[strlen(word) - 1] = '\0';
+		NSLog(@"%s is %d character(s) long, the first character is %c", word, strlen(word), *word);
 	}
+		  fclose(wordFile);
     return 0;
 }
