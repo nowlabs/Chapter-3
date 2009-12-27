7cc0d99dbe6868e3ad5169257aed78da749f335a
Thomas Eapen <thomas@nowlabs.net>
2009-12-27 11:37:10 -0500

    Creating and drawing shapes in Procedural C.

--- Chapter 3.m
+++ Chapter 3.m
@@ -1,16 +1,102 @@
 #import <Foundation/Foundation.h>
 
+typedef	enum {
+	kCircle,
+	kRectangle,
+	kOblateSpheroid
+} ShapeType;
+
+typedef	enum {
+	kRedColor,
+	kGreenColor,
+	kBlueColor
+} ShapeColor;
+
+typedef struct {
+	int x, y, width, height;
+} ShapeRect;
+
+typedef	struct {
+	ShapeType type;
+	ShapeColor fillColor;
+	ShapeRect bounds;
+} Shape;
+
+
+NSString *colorName (ShapeColor colorName) {
+	switch (colorName) {
+		case kRedColor:
+			return @"red";
+			break;
+		case kGreenColor:
+			return @"green";
+			break;
+		case kBlueColor:
+			return @"blue";
+			break;
+		default:
+			return @"undefined color";
+			break;
+	}
+}
+
+void drawCircle(ShapeRect bounds, ShapeColor fillColor) {
+	NSLog(@"drawing a circle at (%d %d %d %d) in %@", bounds.x, bounds.y, bounds.width,
+		  bounds.height, colorName(fillColor));
+}
+
+void drawRectangle(ShapeRect bounds, ShapeColor fillColor) {
+	NSLog(@"drawing a rectangle at (%d %d %d %d) in %@", bounds.x, bounds.y, bounds.width,
+		  bounds.height, colorName(fillColor));
+}
+
+void drawEgg(ShapeRect bounds, ShapeColor fillColor) {
+	NSLog(@"drawing a rectangle at (%d %d %d %d) in %@", bounds.x, bounds.y, bounds.width,
+		  bounds.height, colorName(fillColor));
+}
+
+void drawShapes (Shape shapes[], int count) {
+	int i;
+	for	(i = 0; i < count; i++) {
+		switch (shapes[i].type) {
+			case kCircle:
+				drawCircle(shapes[i].bounds, shapes[i].fillColor);
+				break;
+			case kRectangle:
+				drawRectangle(shapes[i].bounds, shapes[i].fillColor);
+				break;
+			case kOblateSpheroid:
+				drawEgg(shapes[i].bounds, shapes[i].fillColor);
+				break;
+		}
+	}
+}
+
+
 int main (int argc, const char * argv[]) {
-	if (argc == 1) {
-		NSLog(@"you need to provide a file name");
-		return 1;
-	}
-	FILE *wordFile = fopen(argv[1], "r");
-	char word[100];
-	while (fgets(word, 100, wordFile)) {
-		word[strlen(word) - 1] = '\0';
-		NSLog(@"%s is %d character(s) long, the first character is %c", word, strlen(word), *word);
-	}
-		  fclose(wordFile);
+	Shape shapes[3];
+	
+	ShapeRect rect0 = { 0, 0, 10, 30 };
+	shapes[0].type = kCircle;
+	shapes[0].fillColor = kRedColor;
+	shapes[0].bounds = rect0;
+	
+	ShapeRect rect1 = {30, 40, 50,60};
+	shapes[1].type = kRectangle;
+	shapes[1].fillColor = kGreenColor;
+	shapes[1].bounds = rect1;
+	
+	ShapeRect rect2 = {15, 18, 37, 29};
+	shapes[2].type = kOblateSpheroid;
+	shapes[2].fillColor = kBlueColor;
+	shapes[2].bounds = rect2;
+	
+	drawShapes (shapes, 3);
+	
     return 0;
 }
+
+
+
+
+
